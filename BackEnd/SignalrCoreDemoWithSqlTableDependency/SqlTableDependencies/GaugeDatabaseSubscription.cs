26a65dcee44f9e6681c4363b35f4c06f6eaed4a7
IT\peyman <peyman@it-integration.se>
2018-01-25 12:09:46 +0100

    Small refactor

--- BackEnd/SignalrCoreDemoWithSqlTableDependency/SqlTableDependencies/GaugeDatabaseSubscription.cs
+++ BackEnd/SignalrCoreDemoWithSqlTableDependency/SqlTableDependencies/GaugeDatabaseSubscription.cs
@@ -11,7 +11,7 @@ namespace SignalrCoreDemoWithSqlTableDependency.SqlTableDependencies
 {
     public class GaugeDatabaseSubscription : IDatabaseSubscription
     {
-        private bool disposedValue = false;
+        private bool _disposedValue;
         private readonly IGaugeRepository _repository;
         private readonly IHubContext<GaugeHub> _hubContext;
         private SqlTableDependency<Gauge> _tableDependency;
@@ -24,7 +24,7 @@ namespace SignalrCoreDemoWithSqlTableDependency.SqlTableDependencies
 
         public void Configure(string connectionString)
         {
-            _tableDependency = new SqlTableDependency<Gauge>(connectionString, null, null, null, null, DmlTriggerType.All);
+            _tableDependency = new SqlTableDependency<Gauge>(connectionString);
             _tableDependency.OnChanged += Changed;
             _tableDependency.OnError += TableDependency_OnError;
             _tableDependency.Start();
@@ -32,7 +32,7 @@ namespace SignalrCoreDemoWithSqlTableDependency.SqlTableDependencies
             Console.WriteLine("Waiting for receiving notifications...");
         }
 
-        private void TableDependency_OnError(object sender, TableDependency.EventArgs.ErrorEventArgs e)
+        private void TableDependency_OnError(object sender, ErrorEventArgs e)
         {
             Console.WriteLine($"SqlTableDependency error: {e.Error.Message}");
         }
@@ -41,7 +41,6 @@ namespace SignalrCoreDemoWithSqlTableDependency.SqlTableDependencies
         {
             if (e.ChangeType != ChangeType.None)
             {
-                var changedEntity = e.Entity;
                 _hubContext.Clients.All.InvokeAsync("GetGaugesData", _repository.Gauge);
             }
         }
@@ -55,14 +54,14 @@ namespace SignalrCoreDemoWithSqlTableDependency.SqlTableDependencies
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!disposedValue)
+            if (!_disposedValue)
             {
                 if (disposing)
                 {
                     _tableDependency.Stop();
                 }
 
-                disposedValue = true;
+                _disposedValue = true;
             }
         }
 
